867dc4cd655a9e4011554e2eb0f9ffbe69705a02
Lucas Donghoon Lee <yanggun7201@gmail.com>
2018-09-01 15:18:44 +1200

    Finish WeatherItem Component

--- src/components/weather/WeatherItem.tsx
+++ src/components/weather/WeatherItem.tsx
@@ -27,6 +27,16 @@ class WeatherItem extends React.Component<IProps> {
         return <i className={icon} />;
     };
 
+    public isFloat = n => {
+        return Number(n) === n && n % 1 !== 0;
+    };
+
+    public renderTemporature = () => {
+        const data = this.props.data;
+        const temporature = data.main.temp - 273.15;
+        return temporature.toFixed(0) + " °C";
+    };
+
     public render() {
         if (!this.props.data) {
             return null;
@@ -38,16 +48,51 @@ class WeatherItem extends React.Component<IProps> {
             <WeatherContext.Consumer>
                 {context =>
                     context && (
-                        <div className="card">
+                        <div className="card border-dark mb-3">
+                            <div className="card-header">
+                                <h3 className="card-header-title">{data.name}</h3>
+                            </div>
                             <div className="card-body">
-                                <h5 className="card-title">{data.name}</h5>
-                                <p className="card-text">
-                                    With supporting text below as a natural lead-in to additional content.
-                                    {this.renderIcon()}
-                                </p>
-                                <a href="#" className="btn btn-primary">
-                                    Go somewhere
-                                </a>
+                                <div className="row mb-1 mt-1">
+                                    <div className="col-7 weather-icon">{this.renderIcon()}</div>
+                                    <div className="col-5 text-right-display-inherit">
+                                        <div className="row">{this.renderTemporature()}</div>
+                                        <div className="row">{data.weather[0].main}</div>
+                                        <div className="row">
+                                            <i>
+                                                {data.name},{data.sys.country}
+                                            </i>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="card-footer">
+                                <div className="row text-center">
+                                    <div className="col-4">
+                                        <i className="wi wi-strong-wind" />
+                                        <br />
+                                        <span>
+                                            {data.wind.speed}
+                                            &nbsp;m/s
+                                        </span>
+                                    </div>
+                                    <div className="col-4">
+                                        <i className="wi wi-raindrop" />
+                                        <br />
+                                        <span>
+                                            {data.main.humidity}
+                                            &nbsp;%
+                                        </span>
+                                    </div>
+                                    <div className="col-4">
+                                        <i className="wi wi-cloud" />
+                                        <br />
+                                        <span>
+                                            {data.clouds.all}
+                                            &nbsp;%
+                                        </span>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     )
