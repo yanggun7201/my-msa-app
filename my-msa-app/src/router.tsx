288bc7fa903f3ea851c614ebda923960022f7c39
Lucas Donghoon Lee <yanggun7201@gmail.com>
2018-09-11 13:44:44 +1200

    Put Switch to avoid "redirect to the same route" error.

--- my-msa-app/src/router.tsx
+++ my-msa-app/src/router.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { BrowserRouter, Redirect, Route } from "react-router-dom";
+import { BrowserRouter, Redirect, Route, Switch } from "react-router-dom";
 import App from "./App";
 import ForecastPage from "./components/forecast/ForecastPage";
 import { Header } from "./components/Header";
@@ -24,14 +24,16 @@ export const AppRouter: React.StatelessComponent<{}> = () => {
                             <div id="theme-root" className={`theme-${theme}`}>
                                 <Header />
                                 <main>
-                                    <Route exact={true} path="/" component={App} />
-                                    <Route exact={true} path="/theme" component={WithConsumerThemePage} />
-                                    <Route
-                                        exact={true}
-                                        path="/forecast/:cityName"
-                                        component={WithConsumerForecastPage}
-                                    />
-                                    <Redirect from="*" to="/" />
+                                    <Switch>
+                                        <Route exact={true} path="/" component={App} />
+                                        <Route exact={true} path="/theme" component={WithConsumerThemePage} />
+                                        <Route
+                                            exact={true}
+                                            path="/forecast/:cityName"
+                                            component={WithConsumerForecastPage}
+                                        />
+                                        <Redirect from="*" to="/" />
+                                    </Switch>
                                 </main>
                             </div>
                         </BrowserRouter>
